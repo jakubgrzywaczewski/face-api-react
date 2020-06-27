c5b093df7de5f1eefc7c1e6b98dcf85d911c5ce2
enk1 <grzywaczewski.jakub@gmail.com>
2020-06-27 23:34:21 +0200

    Updates

---
+++ precache-manifest.7f71c8c06277edfe380a2660a62ef076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435ea6a8b7edddb28f11024706f0185f",
+    "url": "/face-api-react/index.html"
+  },
+  {
+    "revision": "f062c3098f03f7eefc38",
+    "url": "/face-api-react/static/css/main.700d5ae2.chunk.css"
+  },
+  {
+    "revision": "99206afe23c291255c6d",
+    "url": "/face-api-react/static/js/2.54cdbccc.chunk.js"
+  },
+  {
+    "revision": "d39744b49cd97a6a5d7904677be33b38",
+    "url": "/face-api-react/static/js/2.54cdbccc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f062c3098f03f7eefc38",
+    "url": "/face-api-react/static/js/main.411ddfc9.chunk.js"
+  },
+  {
+    "revision": "b0cbbbc37da27c695605",
+    "url": "/face-api-react/static/js/runtime-main.7fdf9b75.js"
+  }
+]);
